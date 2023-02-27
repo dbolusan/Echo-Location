7a56fce929f80bea94a5e1c5ab27d436ef06619c
bapricha <bapricha@calpoly.edu>
2023-02-26 23:02:35 -0800

    quick fixes for scenes. Only thing not working is text

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -30,55 +30,28 @@ public class ObstacleMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        Quaternion rot = transform.localRotation;
-        
         rigidBody.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
         
-        // if moving right
-        if(horizontalMove > 0 && !facingRight) {
-            ChangeDirection();
+        Quaternion rot = transform.localRotation;
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.localRotation = Quaternion.Euler(0, 0.0f, rot.z);
             horizontalMove = 1.0f;
             facingRight = true;
         }
-        // if moving left
-        else if (horizontalMove < 0 && facingRight) {
-            ChangeDirection();
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.localRotation = Quaternion.Euler(0, 180.0f, rot.z);
             horizontalMove = -1.0f;
             facingRight = false;
-        } 
-        // if moving upward
-        if(verticalMove > 0 && upsideDown) {
-            ChangeDirection();
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.localRotation = Quaternion.Euler(0, rot.y, 90.0f);
             verticalMove = -1.0f;
             upsideDown = false;
         }
-        // if moving downward
-        else if (verticalMove < 0 && !upsideDown) {
-            ChangeDirection();
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.localRotation = Quaternion.Euler(0, rot.y, 270.0f);
             verticalMove = 1.0f;
             upsideDown = true;
         }
-
-        if(Input.GetKey(KeyCode.Space)){
-            transform.position = new Vector2(-8.75f,0);
-            transform.localRotation = Quaternion.Euler(rot.x, rot.y, 0.0f);
-        }
     }
-
-    private void ChangeDirection () {
-        Quaternion rot = transform.localRotation;
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            transform.localRotation = Quaternion.Euler(rot.x, 0.0f, 0);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            transform.localRotation = Quaternion.Euler(rot.x, 180.0f, 0);
-        }
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.localRotation = Quaternion.Euler(rot.x, rot.y, 90.0f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.localRotation = Quaternion.Euler(rot.x, rot.y, 270.0f);
-        }
-     }
 }
-
